8f3c3da6dd5d782977001e5d5f8ea37b40168f20
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-01-03 16:29:14 +0200

    added adding tag functionality

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { useRef, useState } from 'react';
-
+import { Badge } from '@/components/ui/badge';
+import Image from 'next/image';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
@@ -17,6 +18,7 @@ import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 import { QuestionValidation } from '@/lib/validations';
 import { Editor } from '@tinymce/tinymce-react';
+import { type } from 'os';
 
 function onSubmit(values: z.infer<typeof QuestionValidation>) {
   // Do something with the form values.
@@ -33,6 +35,42 @@ const Question = () => {
       tags: [],
     },
   });
+
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: any
+  ) => {
+    if (e.key === 'Enter' && field.name === 'tags') {
+      e.preventDefault();
+
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+
+      if (tagValue !== '') {
+        if (tagValue.length > 15) {
+          return form.setError('tags', {
+            type: 'required',
+            message: 'Tag must be less than 15 characters.',
+          });
+        }
+
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue('tags', [...field.value, tagValue]);
+          tagInput.value = '';
+          form.clearErrors('tags');
+        } else {
+          form.trigger();
+        }
+      }
+    }
+  };
+
+  const handleTagRemove = (tag: string, field: any) => {
+    form.setValue(
+      'tags',
+      field.value.filter((t: string) => t !== tag)
+    );
+  };
 
   return (
     <Form {...form}>
@@ -92,6 +130,60 @@ const Question = () => {
             content_style: 'body { font-family:Inter; font-size:16px }',
           }}
           initialValue='Welcome to TinyMCE!'
+        />
+
+        <FormField
+          control={form.control}
+          name='tags'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col'>
+              <FormLabel className='paragraph-semibold text-dark400_light800'>
+                Tags <span className='text-primary-500'>*</span>
+              </FormLabel>
+              <FormControl className='mt-3.5'>
+                <>
+                  <Input
+                    disabled={type === 'Edit'}
+                    className=' paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border'
+                    placeholder='Add tags...'
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+
+                  {field.value.length > 0 && (
+                    <div className='flex-start mt-2.5 gap-2.5'>
+                      {field.value.map((tag: any) => (
+                        <Badge
+                          key={tag}
+                          className='subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize'
+                          onClick={() =>
+                            type !== 'Edit'
+                              ? handleTagRemove(tag, field)
+                              : () => {}
+                          }
+                        >
+                          {tag}
+                          {type !== 'Edit' && (
+                            <Image
+                              src='/assets/icons/close.svg'
+                              alt='Close icon'
+                              width={12}
+                              height={12}
+                              className='cursor-pointer object-contain invert-0 dark:invert'
+                            />
+                          )}
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
+              </FormControl>
+              <FormDescription className='body-regular mt-2.5 text-light-500'>
+                Add up to 3 tags to describe what your question is about. You
+                need to press enter to add a tag.
+              </FormDescription>
+              <FormMessage className='text-red-500' />
+            </FormItem>
+          )}
         />
         <Button className='w-full' type='submit'>
           Submit
